cd8bd1ba925829a998d708fb4f1a970f07c5fc5d
ArseniyAsriyan <80623657+ArseniyAsriyan@users.noreply.github.com>
2021-05-03 12:27:57 +0300

    1.1.3 Completed task

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,13 +7,15 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?serverTimezone=UTC";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     public static Connection getSQLConnection() {
         Connection connection = null;
         try {
+            Class.forName(DRIVER);
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
-        } catch (SQLException e) {
+        } catch (Exception e) {
             System.err.println("Не удалось установить соединение с базой данных");
             e.printStackTrace();
         }
